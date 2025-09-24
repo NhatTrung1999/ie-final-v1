b33919572efc78309524438cac2e7acb20305d21
Trung Nguyen <trungnguyen15051999@gmail.com>
2025-09-24 17:55:47 +0700

    'fix-bug-20250924'

--- backend/src/modules/stagelist/stagelist.module.ts
+++ backend/src/modules/stagelist/stagelist.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StagelistService } from './stagelist.service';
 import { StagelistController } from './stagelist.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [StagelistController],
   providers: [StagelistService],
 })
--- backend/src/modules/stagelist/stagelist.service.ts
+++ backend/src/modules/stagelist/stagelist.service.ts
@@ -1,21 +1,90 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateStagelistDto } from './dto/create-stagelist.dto';
+import * as path from 'path';
+import * as fs from 'fs';
+import { Sequelize } from 'sequelize-typescript';
+import { QueryTypes } from 'sequelize';
+import { v4 as uuidv4 } from 'uuid';
+import { IStageListData } from 'src/types/stagelist';
 
 @Injectable()
 export class StagelistService {
+  constructor(@Inject('IE') private readonly IE: Sequelize) {}
+
   async stagelistUpload(
     createStagelistDto: CreateStagelistDto,
     files: Array<Express.Multer.File>,
   ) {
+    const id = uuidv4();
     const { date, season, stage, area, article } = createStagelistDto;
-    console.log(date, season, stage, area, article, files);
-    return {
+    const resData: IStageListData[] = [];
+
+    const basePath = path.join(
+      process.cwd(),
+      'uploads',
       date,
       season,
       stage,
       area,
       article,
-      files: files[0].path,
-    };
+    );
+    if (!fs.existsSync(basePath)) {
+      fs.mkdirSync(basePath, { recursive: true });
+    }
+    for (let item of files) {
+      const filePath = path.join(basePath, item.originalname);
+      // console.log(filePath);
+      await this.IE.query(
+        `INSERT INTO IE_StageList
+        (
+          Id,
+          [Date],
+          Season,
+          Stage,
+          Area,
+          Article,
+          Name,
+          [Path],
+          CreatedBy,
+          CreatedFactory,
+          CreatedAt
+        )
+        VALUES
+        (
+          ?,
+          ?,
+          ?,
+          ?,
+          ?,
+          ?,
+          ?,
+          ?,
+          ?,
+          ?,
+          GETDATE()
+        )`,
+        {
+          replacements: [
+            id,
+            date,
+            season,
+            stage,
+            area,
+            article,
+            item.originalname,
+            filePath,
+            'admin',
+            'LYV',
+          ],
+          type: QueryTypes.INSERT,
+        },
+      );
+
+      const [result] = await this.IE.query(
+        `SELECT * FROM IE_StageList WHERE Id = ?`,
+        { replacements: [id], type: QueryTypes.SELECT },
+      );
+      // resData.push(result)
+    }
   }
 }
